b56eb01ab9366921fc444d576cf329d50aca628c
morrison <morrisonwanja@gmail.com>
2023-12-04 19:01:01 +0300

    Complete Lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
-def write_file(file_name, file_content):
+
+
+
+def write_file(file_name="test_file.txt", file_content="Log 1: We are adding five bananas to our file."):
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
     pass
 
-def append_file(file_name, append_content):
+def append_file(file_name="test_file", append_content="We are adding five more potatoes and yams to the lgofile."):
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
     pass
 
-def read_file(file_name):
+def read_file(file_name="test_file"):
+    with open(f'{file_name}.txt', 'r') as f:
+        return f.read()
     pass
